8d3f2996724d03be6f448755d47c351e23647432
panteliskts <146824288+panteliskts@users.noreply.github.com>
2024-07-23 03:23:49 +0300

    Create strcpy.c

---
+++ string copier/code.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<string.h>
+ 
+void copy(char* dst, char* src){
+
+  while(*src != '\0'){
+
+strcpy(dst , src);
+src++;
+dst++;
+  }
+  *dst= '\0';
+}
+ 
+int main(){
+  char srcString[] = "We promptly judged antique ivory buckles for the next prize!";
+
+
+
+
+
+  char dstString[strlen(srcString) + 1];
+
+copy(dstString,srcString );
+
+printf("%s\n",dstString );
+
+
+ 
+}
