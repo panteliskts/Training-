1ec8872403a8e8c222589aca1475939e64d477dc
Pantelis Kotsas <146824288+panteliskts@users.noreply.github.com>
2024-10-02 00:24:11 +0300

    Create dogyears.cpp

---
+++ c++/dogyears.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+  int dog_age; 
+  int early_years=21;
+  int later_years= (dog_age -2)*4;
+  int human_years= early_years + later_years;
+
+  
+  
+std::cout<< "My name is Ruff ruff, I am " << human_years << " years old in human years.\n";
+  
+}
